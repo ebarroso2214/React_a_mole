ee5faaf08e7ea941863bef658fc82e490c5f954c
ebarroso2214 <ebarroso2214@gmail.com>
2023-04-18 20:57:31 -0400

    created whack a mole game, moles get clicked on and randomly pop in and out randomly. when moles get clicked, score counter goes up by 1

--- src/Components/MoleContainer.js
+++ src/Components/MoleContainer.js
@@ -2,10 +2,11 @@ import { useState } from "react";
 import Mole from "./Mole";
 import EmptySlot from "./EmptySlot";
 
+
 function MoleContainer(props) {
     const [displayMole, setDisplayMole] = useState(false)
 
-    const handleClick = (e) => {
+    const handleClick = (e) => { //Function to handle when mole is clicked the score is set +1 and the display turns to false.
         props.setScore(props.score + 1)
         setDisplayMole(false)
     }
